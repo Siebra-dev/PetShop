c1aa8c9cd1de9d62533389a6123713d4ff67eb49
Renan Siebra Braz <r.siebra@aluno.ifsp.edu.br>
2026-03-18 08:26:53 -0300

    ajustando menu

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from 'express';
+import { createMenuObject } from '../helpers/createMenuObject';
 
 export const home = (req: Request, res: Response) => {
-    //res.send('home');
-    res.render('pages/page', {
+   res.render('pages/page', {
+    menu: {
+        all: true,
+        dogs: false,
+        cats: false,
+        fishes: false
+    },
         banner: {
             title: 'Todos os animais',
             background: 'allanimals.jpg'
@@ -11,13 +17,46 @@ export const home = (req: Request, res: Response) => {
 };
 
 export const dogs = (req: Request, res: Response) => {
-    res.render('pages/page');
+    res.render('pages/page', {
+        menu: {
+        all: false,
+        dogs: true,
+        cats: false,
+        fishes: false
+    },
+        banner: {
+            title: 'Cães',
+            background: 'banner_dog.jpg'
+        }
+    });
 };
 
 export const cats = (req: Request, res: Response) => {
-    res.render('pages/page');
+    res.render('pages/page', {
+        menu: {
+        all: false,
+        dogs: false,
+        cats: true,
+        fishes: false
+    },
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    });
 };
 
 export const fishes = (req: Request, res: Response) => {
-    res.render('pages/page');
+    res.render('pages/page', {
+        menu: {
+        all: false,
+        dogs: false,
+        cats: false,
+        fishes: true
+    },
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    });
 };
