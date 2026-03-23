2955424f5fc7b0c6a0534d7adbd6dd8b8ee3b39e
Renan Siebra Braz <r.siebra@aluno.ifsp.edu.br>
2026-03-23 08:40:09 -0300

    Recuperando arquivos do projeto

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from 'express';
+import { createMenuObject } from '../helpers/createMenuObject';
+
+export const home = (req: Request, res: Response) => {
+   res.render('pages/page', {
+    menu: {
+        all: true,
+        dogs: false,
+        cats: false,
+        fishes: false
+    },
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    });
+};
+
+export const dogs = (req: Request, res: Response) => {
+    res.render('pages/page', {
+        menu: {
+        all: false,
+        dogs: true,
+        cats: false,
+        fishes: false
+    },
+        banner: {
+            title: 'Cães',
+            background: 'banner_dog.jpg'
+        }
+    });
+};
+
+export const cats = (req: Request, res: Response) => {
+    res.render('pages/page', {
+        menu: {
+        all: false,
+        dogs: false,
+        cats: true,
+        fishes: false
+    },
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    });
+};
+
+export const fishes = (req: Request, res: Response) => {
+    res.render('pages/page', {
+        menu: {
+        all: false,
+        dogs: false,
+        cats: false,
+        fishes: true
+    },
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    });
+};
